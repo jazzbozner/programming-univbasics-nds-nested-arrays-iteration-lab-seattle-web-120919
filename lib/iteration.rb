6a2cd290485235039df2fccde659deee5fcaf059
Jazz Bozner <jazzbozner@gmail.com>
2020-01-07 21:55:03 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -21,20 +21,20 @@ def find_greater_pair(arr)
       result.push(arr[rowindex][1])
     end
     row_index += 1
-end
-result
+  end
+  result
 end
 
 
-#   result = []
-#   row_index = 0
-#   while row_index < src.count do
-#     if src[row_index][0] >= src[row_index][1]
-#       result.push(src[row_index][0])
-#     else
-#       result.push(src[row_index][1])
-#     end
-#     row_index += 1
-#   end
-#   result
-# end
+  result = []
+  row_index = 0
+  while row_index < src.count do
+    if src[row_index][0] >= src[row_index][1]
+      result.push(src[row_index][0])
+    else
+      result.push(src[row_index][1])
+    end
+    row_index += 1
+  end
+  result
+end
