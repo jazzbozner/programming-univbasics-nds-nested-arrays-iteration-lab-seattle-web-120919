84f1a8de31cd1c157d96b755bdc6ad235dd01441
Jazz Bozner <jazzbozner@gmail.com>
2020-01-07 21:40:47 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -4,7 +4,7 @@ def join_ingredients(arr)
   result = []
   inner_index = 0
   while inner_index < arr.length do
-    result.push("I love" + arr[inner_index][0] + " and " +arr[inner_index][1] + "on my pizza")
+    result.push("I love " + arr[inner_index][0] + " and " +arr[inner_index][1] + "on my pizza")
     inner_index += 1
   end
   result
